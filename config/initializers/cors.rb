d3e8f67a70ba79d6badaccb99000c739bd7f5b68
Michael Kielpinski <kielpinski.michael@gmail.com>
2022-12-08 03:31:04 -0800

    maybe this broke it

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,7 +4,7 @@
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
 # Read more: https://github.com/cyu/rack-cors
-Rails.application.config.hosts << ENV['ORIGIN']
+# Rails.application.config.hosts << ENV['ORIGIN']
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
